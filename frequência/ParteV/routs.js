ee8ee02fc771dad41a5fc337e5e8095d66e9418b
iinesmmartins <118077893+iinesmmartins@users.noreply.github.com>
2024-06-03 11:16:17 +0100

    test1

--- frequência/ParteV/routs.js
+++ frequência/ParteV/routs.js
@@ -1,6 +1,11 @@
 const productsRouter = require('express').Router();
 const controller = require('../controllers/products');
 const authMiddleware  =require('../middlewares/auth/auth');
+productsRouter.get('/', controller.getAll); //le todos
+productsRouter.get('/:id', controller.getById); //le 1 produto pelo id
+productsRouter.post('/create', controller.create); //criar um novo produto
+productsRouter.put('/update', controller.update); //atualizar um produto
+productsRouter.delete('/delete/:id', controller.delete); //apagar um protudo pelo id
 
 
 module.exports= productsRouter;
